e79127654fd5bf3e34e66507624fb1748a942da4
Oleg Dneprovsky <lovedesignua@gmail.com>
2024-04-24 19:37:26 +0700

    add google auth, login and logout btns

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,30 +5,36 @@ import Header from "@/components/Header";
 import { Toaster } from "@/components/ui/sonner";
 
 import { cn } from "@/lib/utils";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <Header />
-        <main className="container pt-6">{children}</main>
+  const session = await auth();
 
-        <Toaster duration={4000} />
-      </body>
-    </html>
+  return (
+    <SessionProvider session={session}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Header />
+          <main className="container pt-6">{children}</main>
+
+          <Toaster duration={4000} />
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Bug } from "lucide-react";
+import { Bug, LogIn, LogOut } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+import PulseLoader from "react-spinners/PulseLoader";
 
 const links = [
   { href: "/dashboard", label: "Dashboard" },
@@ -12,27 +15,52 @@ const links = [
 
 export default function Header() {
   const pathname = usePathname();
+  const { data, status } = useSession();
 
   return (
-    <nav className="container py-4 flex items-center gap-6 border-b">
-      <Link href="/">
-        <Bug />
-      </Link>
+    <nav className="container py-4 flex items-center justify-between gap-6 border-b">
+      <div className="flex irems-center gap-6">
+        <Link href="/">
+          <Bug />
+        </Link>
 
-      <ul className="flex gap-6">
-        {links.map(({ href, label }) => (
-          <li key={label}>
-            <Link
-              href={href}
-              className={cn("font-medium text-foreground/75", {
-                "font-semibold text-foreground": href === pathname,
-              })}
-            >
-              {label}
+        <ul className="flex gap-6">
+          {links.map(({ href, label }) => (
+            <li key={label}>
+              <Link
+                href={href}
+                className={cn("font-medium text-foreground/75", {
+                  "font-semibold text-foreground": href === pathname,
+                })}
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        {status === "authenticated" ? (
+          <Button className="w-[112px]" variant="outline" asChild>
+            <Link href="/api/auth/signout" onClick={() => signOut()}>
+              <LogIn className="size-3.5 mr-2" />
+              Sign out
             </Link>
-          </li>
-        ))}
-      </ul>
+          </Button>
+        ) : status === "loading" ? (
+          <Button className="w-[112px]" variant="outline">
+            <PulseLoader size={8} />
+          </Button>
+        ) : (
+          <Button className="w-[112px]" variant="outline" asChild>
+            <Link href="/api/auth/signin" onClick={() => signIn()}>
+              <LogOut className="size-3.5 mr-2" />
+              Sign in
+            </Link>
+          </Button>
+        )}
+      </div>
     </nav>
   );
 }
